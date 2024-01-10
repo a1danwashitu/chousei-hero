c22c92efb01a9ffe4cd626a2a4873f3589c2bde4
kyamotsu <kyamotu_kyamotu@yahoo.co.jp>
2024-01-10 21:53:06 +0900

    test gh-pages sample

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
+	c := make(chan struct{})
+
 	fmt.Println("Hello, World!")
+	registerCallbacks()
+	<-c
+}
+
+func registerCallbacks() {
+	js.Global().Set("output", js.FuncOf(output))
+}
+
+func output(this js.Value, args []js.Value) interface{} {
+	text := textToStr(args[0])
+
+	outputToHtml(text)
+	return nil
+}
+
+func textToStr(v js.Value) string {
+    return js.Global().Get("document").Call("getElementById", v.String()).Get("value").String()
+}
+
+func outputToHtml(text string) {
+    println("print:", text)
+    js.Global().Get("document").Call("getElementById", "output").Set("innerHTML", text)
 }
